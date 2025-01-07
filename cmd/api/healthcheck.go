2d42d241cfce14074f7a1fecb0206dc2fe039ab7
amanycodes <amanycodes@gmail.com>
2025-01-08 00:05:16 +0530

    encoded structs and made JSON responese better

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
@@ -10,20 +9,15 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 	// fmt.Fprintf(w, "environment: %s\n", app.config.env)
 	// fmt.Fprintf(w, "version: %s\n", version)
 
-	data := map[string]string{
+	env := envelope{
 		"status":      "available",
 		"environment": app.config.env,
 		"version":     version,
 	}
 
-	js, err := json.Marshal(data)
+	err := app.writeJSON(w, http.StatusOK, env, nil)
 	if err != nil {
 		app.logger.Print(err)
 		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
-		return
 	}
-
-	js = append(js, '\n')
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
 }
