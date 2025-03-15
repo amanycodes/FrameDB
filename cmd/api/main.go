8e0a7f0cac4813b3adf5fd0b380e9dea3e831e16
amanycodes <amanycodes@gmail.com>
2025-03-16 04:13:07 +0530

    added cors policiies
    Signed-off-by: amanycodes <amanycodes@gmail.com>

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"flag"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -37,6 +38,9 @@ type config struct {
 		password string
 		sender   string
 	}
+	cors struct {
+		trustedOrigins []string
+	}
 }
 
 type application struct {
@@ -68,6 +72,12 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "fb0f371ed26d09", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "157ca367dff742", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "FrameDB <amanycodes@gmail.com>", "SMTP sender")
+
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space seperated)", func(s string) error {
+		cfg.cors.trustedOrigins = strings.Fields(s)
+		return nil
+	})
+
 	flag.Parse()
 
 	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -155,7 +155,6 @@ func (app *application) requireActivatedUser(next http.HandlerFunc) http.Handler
 	return app.requireAuthenticatedUser(fn)
 }
 
-// user permission
 func (app *application) requirePermission(code string, next http.HandlerFunc) http.HandlerFunc {
 
 	fn := func(w http.ResponseWriter, r *http.Request) {
@@ -178,11 +177,28 @@ func (app *application) requirePermission(code string, next http.HandlerFunc) ht
 	return app.requireActivatedUser(fn)
 }
 
-// //ENABLE CORS IMPLEMENATION
 func (app *application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Vary", "Origin")
+		w.Header().Add("Vary", "Access-Control-Request-Method")
 
+		origin := r.Header.Get("Origin")
+		if origin != "" {
+			for i := range app.config.cors.trustedOrigins {
+				if origin == app.config.cors.trustedOrigins[i] {
+					w.Header().Set("Access-Control-Allow-Origin", origin)
+
+					if r.Method == http.MethodOptions && r.Header.Get("Access-Control-Request-Method") != "" {
+						w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, PUT, PATCH, DELETE")
+						w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+
+						w.WriteHeader(http.StatusOK)
+						return
+					}
+					break
+				}
+			}
+		}
 		next.ServeHTTP(w, r)
 	})
 }
